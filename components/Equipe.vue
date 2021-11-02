521a15bfba15bb4a9f03256e339bd29e0c037bf2
Mikael Bezerra <69210563+MikaelBezerra@users.noreply.github.com>
2021-11-02 16:56:32 -0300

    Add files via upload

---
+++ components/Equipe.vue
@@ -0,0 +1,165 @@
+<template>
+  <section
+    class="py-20 xl:bg-contain bg-top bg-no-repeat"
+    style="background-image: url('metis-assets/backgrounds/intersect.svg')"
+    id="equipe"
+  >
+    <div class="container px-4 mx-auto">
+      <div class="max-w-lg mx-auto mb-12 text-center">
+        <img class="mx-auto" src="metis-assets/icons/quote.svg" alt="" />
+        <h2 class="my-2 text-3xl md:text-4xl font-bold font-heading">Equipe</h2>
+        <p class="text-blueGray-400 leading-loose">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed luctus
+          eget justo et iaculis.
+        </p>
+      </div>
+      <div class="flex flex-wrap max-w-5xl mx-auto mb-6">
+        <div class="w-full md:w-1/2 px-3 mb-6">
+          <div class="p-8 bg-white shadow rounded">
+            <div class="flex items-center mb-4">
+              <img
+                class="h-16 w-16 rounded-full object-cover"
+                src="https://github.com/MikaelBezerra.png"
+                alt=""
+              />
+              <div class="pl-4">
+                <p class="text-xl">Mikael Bezerra</p>
+                <p class="text-blue-600">Front-end</p>
+              </div>
+            </div>
+            <p class="leading-loose text-blueGray-400">lorem</p>
+          </div>
+        </div>
+        <div class="w-full md:w-1/2 px-3 mb-6">
+          <div class="p-8 bg-white shadow rounded">
+            <div class="flex items-center mb-4">
+              <img
+                class="h-16 w-16 rounded-full object-cover"
+                src="https://github.com/PauloCalletti.png"
+                alt=""
+              />
+              <div class="pl-4">
+                <p class="text-xl">Paulo Tonhão</p>
+                <p class="text-blue-600">Back-end</p>
+              </div>
+            </div>
+            <p class="leading-loose text-blueGray-400">lorem</p>
+          </div>
+        </div>
+        <div class="w-full md:w-1/2 px-3 mb-6">
+          <div class="p-8 bg-white shadow rounded">
+            <div class="flex items-center mb-4">
+              <img
+                class="h-16 w-16 rounded-full object-cover"
+                src="alice.jpeg"
+                alt=""
+              />
+              <div class="pl-4">
+                <p class="text-xl">Alice Trajeto</p>
+                <p class="text-blue-600">Marketing</p>
+              </div>
+            </div>
+            <p class="leading-loose text-blueGray-400">lorem</p>
+          </div>
+        </div>
+        <div class="w-full md:w-1/2 px-3 mb-6">
+          <div class="p-8 bg-white shadow rounded">
+            <div class="flex items-center mb-4">
+              <img
+                class="h-16 w-16 rounded-full object-cover"
+                src="isis.jpeg"
+                alt=""
+              />
+              <div class="pl-4">
+                <p class="text-xl">Isis</p>
+                <p class="text-blue-600">Negocio</p>
+              </div>
+            </div>
+            <p class="leading-loose text-blueGray-400">lorem</p>
+          </div>
+        </div>
+        <div class="w-full md:w-1/2 px-3 mb-6">
+          <div class="p-8 bg-white shadow rounded">
+            <div class="flex items-center mb-4">
+              <img
+                class="h-16 w-16 rounded-full object-cover"
+                src="soseiquenadasei.jpeg"
+                alt=""
+              />
+              <div class="pl-4">
+                <p class="text-xl">Gustavo</p>
+                <p class="text-blue-600">Negocio</p>
+              </div>
+            </div>
+            <p class="leading-loose text-blueGray-400">lorem</p>
+          </div>
+        </div>
+        <div class="w-full md:w-1/2 px-3 mb-6">
+          <div class="p-8 bg-white shadow rounded">
+            <div class="flex items-center mb-4">
+              <img
+                class="h-16 w-16 rounded-full object-cover"
+                src="playertauz.jpeg "
+                alt=""
+              />
+              <div class="pl-4">
+                <p class="text-xl">Lucas</p>
+                <p class="text-blue-600">Negocio</p>
+              </div>
+            </div>
+            <p class="leading-loose text-blueGray-400">lorem</p>
+          </div>
+        </div>
+        <div class="w-full md:w-1/2 px-3 mb-6">
+          <div class="p-8 bg-white shadow rounded">
+            <div class="flex items-center mb-4">
+              <img
+                class="h-16 w-16 rounded-full object-cover"
+                src="careca.jpeg"
+                alt=""
+              />
+              <div class="pl-4">
+                <p class="text-xl">João Messias</p>
+                <p class="text-blue-600">Back-end</p>
+              </div>
+            </div>
+            <p class="leading-loose text-blueGray-400">lorem</p>
+          </div>
+        </div>
+        <div class="w-full md:w-1/2 px-3 mb-6">
+          <div class="p-8 bg-white shadow rounded">
+            <div class="flex items-center mb-4">
+              <img
+                class="h-16 w-16 rounded-full object-cover"
+                src="raiany.jpeg"
+                alt=""
+              />
+              <div class="pl-4">
+                <p class="text-xl">Raiany</p>
+                <p class="text-blue-600">Negocio</p>
+              </div>
+            </div>
+            <p class="leading-loose text-blueGray-400">lorem</p>
+          </div>
+        </div>
+      </div>
+      <div class="text-center">
+        <a
+          class="
+            inline-block
+            py-4
+            px-8
+            text-xs text-white
+            font-semibold
+            leading-none
+            bg-blue-600
+            hover:bg-blue-700
+            rounded
+          "
+          href="#"
+          >Conhecer mais</a
+        >
+      </div>
+    </div>
+  </section>
+</template>
